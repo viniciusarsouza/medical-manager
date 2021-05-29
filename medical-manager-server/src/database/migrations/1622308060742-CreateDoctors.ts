5e3a15e0e283428cb80314183fd3594633356ca2
viniarsouza <viniciusarsouza@gmail.com>
2021-05-29 14:21:59 -0300

    Creating database connection

---
+++ medical-manager-server/src/database/migrations/1622308060742-CreateDoctors.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateDoctors1622308060742 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
