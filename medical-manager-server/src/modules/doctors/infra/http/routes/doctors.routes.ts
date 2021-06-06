5c19cb690007437a28a64c6e3d750caafe16f8ca
viniarsouza <viniciusarsouza@gmail.com>
2021-06-06 15:01:54 -0300

    Creating data validations

--- medical-manager-server/src/modules/doctors/infra/http/routes/doctors.routes.ts
+++ medical-manager-server/src/modules/doctors/infra/http/routes/doctors.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import DoctorsController from '../controllers/DoctorsController';
 import ListDoctorsController from '../controllers/ListDoctorsController';
@@ -7,26 +8,110 @@ const doctorsRouter = Router();
 const doctorsController = new DoctorsController();
 const listDoctorsController = new ListDoctorsController();
 
-doctorsRouter.post('/', doctorsController.create);
+doctorsRouter.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().max(120).required(),
+            crm: Joi.number().max(10000000).required(),
+            landline: Joi.number().required(),
+            cellphone: Joi.number().required(),
+            cep: Joi.number().max(100000000).required(),
+            first_medical_specialty: Joi.string().required(),
+            second_medical_specialty: Joi.string(),
+        },
+    }),
+    doctorsController.create,
+);
 
 doctorsRouter.get('/', doctorsController.index);
 
-doctorsRouter.get('/:id', doctorsController.getById);
+doctorsRouter.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    doctorsController.getById,
+);
 
-doctorsRouter.delete('/:id', doctorsController.delete);
+doctorsRouter.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    doctorsController.delete,
+);
 
-doctorsRouter.put('/:id', doctorsController.update);
+doctorsRouter.put(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+        [Segments.BODY]: {
+            name: Joi.string().max(120).required(),
+            crm: Joi.number().max(10000000).required(),
+            landline: Joi.number().required(),
+            cellphone: Joi.number().required(),
+            cep: Joi.number().max(100000000).required(),
+            first_medical_specialty: Joi.string().required(),
+            second_medical_specialty: Joi.string(),
+        },
+    }),
+    doctorsController.update,
+);
 
-doctorsRouter.get('/list/cep/:cep', listDoctorsController.indexByCep);
+doctorsRouter.get(
+    '/list/cep/:cep',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().required(),
+        },
+    }),
+    listDoctorsController.indexByCep,
+);
 
-doctorsRouter.get('/list/street/:street', listDoctorsController.indexByStreet);
+doctorsRouter.get(
+    '/list/street/:street',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().required(),
+        },
+    }),
+    listDoctorsController.indexByStreet,
+);
 
-doctorsRouter.get('/list/city/:city', listDoctorsController.indexByCity);
+doctorsRouter.get(
+    '/list/city/:city',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().required(),
+        },
+    }),
+    listDoctorsController.indexByCity,
+);
 
-doctorsRouter.get('/list/state/:state', listDoctorsController.indexByState);
+doctorsRouter.get(
+    '/list/state/:state',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().required(),
+        },
+    }),
+    listDoctorsController.indexByState,
+);
 
 doctorsRouter.get(
     '/list/medicalspecialty/:medicalSpecialty',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().required(),
+        },
+    }),
     listDoctorsController.indexByMedicalSpecialty,
 );
 
--- medical-manager-server/src/shared/infra/http/app.ts
+++ medical-manager-server/src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -12,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(errors());
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
