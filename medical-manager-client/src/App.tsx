6b44163533cdbe84c2f13abfd9662676784feb52
viniarsouza <viniciusarsouza@gmail.com>
2021-06-05 23:22:36 -0300

    Creating frontend initial structure

---
+++ medical-manager-client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GlobalStyle from './global';
+
+const App: React.FC = () => (
+    <>
+        <h1>Hello World</h1>
+        <GlobalStyle />
+    </>
+);
+
+export default App;
